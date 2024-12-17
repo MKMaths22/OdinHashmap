353685499241802923b7a37ecbdc621aebc6a0dd
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-12-17 15:04:58 +0000

    Write HashMapTest#setup to set the 12 key/value pairs given in the Odin lesson. Use the @hash_of_twelve in #test_has_key and #test_length.

--- tests.rb
+++ tests.rb
@@ -3,9 +3,20 @@ require './linkedlist'
 require 'minitest/autorun'
 
 class HashMapTest < Minitest::Test
-    def test_creating_simple_hash
-        hash = HashMap.new
-        hash.set('name', 'Peter')
-        assert(hash.has?('name'), 'Failed to find key')
+  def setup
+    @hash_of_twelve = HashMap.new
+    keys_array = %w[apple banana carrot dog elephant frog grape hat ice\ cream jacket kite lion]
+    values_array = %w[red yellow orange brown grey green purple black white blue pink golden]
+    keys_array.each_with_index do |key, index|
+        @hash_of_twelve.set(key, values_array[index])
     end
+  end
+    
+  def test_has_key
+    assert(@hash_of_twelve.has?('apple'), 'Failed to find key')
+  end
+
+  def test_length
+    assert_equal(@hash_of_twelve.length, 12, 'Length not correct')
+  end
 end
