95b97feb8e63c9766421fd7d8a332b58414d2f0e
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-12-18 16:43:06 +0000

    Fill out more basic method tests, also adding @hash_of_thirteen to HashMapTest#setup to test methods on hashes before and after the buckets are grown.

--- tests.rb
+++ tests.rb
@@ -5,23 +5,66 @@ require 'minitest/autorun'
 class HashMapTest < Minitest::Test
   def setup
     @hash_of_twelve = HashMap.new
+    @hash_of_thirteen = HashMap.new
     @keys_array = %w[apple banana carrot dog elephant frog grape hat ice\ cream jacket kite lion]
     @values_array = %w[red yellow orange brown grey green purple black white blue pink golden]
     @keys_array.each_with_index do |key, index|
         @hash_of_twelve.set(key, @values_array[index])
+        @hash_of_thirteen.set(key, @values_array[index])
     end
+    @hash_of_thirteen.set('moon', 'silver')
   end
     
-  def test_default_capacity
-    assert_equal(@hash_of_twelve.capacity, 16)
+  def test_get_with_present_key 
+    assert_equal(@hash_of_twelve.get(@keys_array[6]), @values_array[6], 'Failed to retrieve correct value')
+    assert_equal(@hash_of_thirteen.get('moon'), 'silver', 'Failed to retrieve correct value')
+  end
+
+  def test_get_with_absent_key
+    assert_nil(@hash_of_twelve.get('moon'), 'Did not give nil value when getting absent key')
   end
   
-  def test_has_key
-    assert(@hash_of_twelve.has?('apple'), 'Failed to find key')
+  def test_capacity
+    assert_equal(@hash_of_twelve.capacity, 16, 'Capacity not 16 with length 12')
+    assert_equal(@hash_of_thirteen.capacity, 32, 'Capacity not 32 with length 13')
+  end
+  
+  def test_has_present_key
+    assert(@hash_of_twelve.has?(@keys_array[0]), 'Failed to find key')
+    assert(@hash_of_thirteen.has?('moon'), 'Failed to find key after capacity doubled')
+  end
+
+  def test_has_absent_key
+    assert_equal(@hash_of_twelve.has?('moon'), false, 'Found absent key')
+    assert_equal(@hash_of_thirteen.has?('Peter'), false, 'Found absent key')
+  end
+
+  def remove_present_key
+
+  end
+
+  def remove_absent_key
+
   end
 
   def test_length
     assert_equal(@hash_of_twelve.length, 12, 'Length not correct')
+  end
+
+  def test_clear
+
+  end
+
+  def test_keys
+
+  end
+
+  def test_values
+
+  end
+
+  def test_entries
+
   end
 
   def test_overwrite_maintains_length
